3001e24bbf1a2651c1128c1ce287934c4c421177
Artem Daugel-Dauge <artem.daugel-dauge@jetbrains.com>
2023-11-16 10:36:52 +0100

    WIP

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,36 +1,32 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+import org.jetbrains.kotlin.konan.target.Family
+
 plugins {
     kotlin("multiplatform")
-    kotlin("native.cocoapods")
+    id("co.touchlab.kmmbridge") version "0.5.1"
     id("com.android.library")
+    `maven-publish`
 }
 
-@OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
 kotlin {
-    targetHierarchy.default()
-
-    android {
+    androidTarget {
         compilations.all {
             kotlinOptions {
                 jvmTarget = "1.8"
             }
         }
     }
+
     iosX64()
     iosArm64()
     iosSimulatorArm64()
 
-    cocoapods {
-        summary = "Some description for the Shared Module"
-        homepage = "Link to the Shared Module homepage"
-        version = "1.0"
-        ios.deploymentTarget = "14.1"
-        podfile = project.file("../iosApp/Podfile")
-        framework {
-            baseName = "shared"
+    targets.withType<KotlinNativeTarget>()
+        .matching { it.konanTarget.family == Family.IOS }
+        .configureEach {
+            binaries.framework {}
         }
 
-        pod("Base64", version = "1.1.2")
-    }
     
     sourceSets {
         commonTest {
@@ -42,10 +38,29 @@ kotlin {
 
 }
 
+version = "0.0.1"
+
 android {
     namespace = "com.daugeldauge.wizardly"
     compileSdk = 33
     defaultConfig {
         minSdk = 33
     }
+}
+
+publishing {
+    repositories {
+        maven {
+            url = uri("https://maven.pkg.github.com/daugeldauge/wizardly")
+            credentials {
+                username = "daugeldauge"
+                password = "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"
+            }
+        }
+    }
+}
+
+kmmbridge {
+    spm()
+    mavenPublishArtifacts()
 }
